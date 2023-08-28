f4efd303a627f204923ca14c85896292f207b334
Gabriel Rocha <101814874+GabrielTrindade20@users.noreply.github.com>
2023-08-27 21:04:27 -0300

    primeiro comit
    adicionando trabalhos

---
+++ Projeto_Livro_Receita/configuration/connect.php
@@ -0,0 +1,10 @@
+<!-- Conexão com o banco de dados --->
+<?php
+
+    define('HOST', 'localhost');
+    define('DATABASENAME', 'livro_receita_dev');
+    define('USER', 'adm_LivroReceitas');
+    define('PASSAWORD', 'livro123');
+
+
+?>
