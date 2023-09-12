ab50fc6b62464e87f3c1764bf6d92977017e5e4a
Patrícia Reis <107223827+PatriciaSuReis@users.noreply.github.com>
2023-09-12 10:12:43 -0300

    Update script_user_dev.sql

--- banco_de_dados_info/script_user_dev.sql
+++ banco_de_dados_info/script_user_dev.sql
@@ -1,2 +1,2 @@
 CREATE USER 'adm_LivroReceitas'@'localhost' IDENTIFIED WITH mysql_native_password BY 'livro123'; 
-grant select, insert, update, delete on eajuda.* to 'adm_LivroReceitas'@'localhost';
+grant select, insert, update, delete on livro_receita_dev.* to 'adm_LivroReceitas'@'localhost';
