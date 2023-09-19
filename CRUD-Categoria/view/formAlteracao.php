d6f25e9ca1fd70282f0235aafcddea3a2cc72c31
unknown <patriciasuellen05@gmail.com>
2023-09-18 21:20:03 -0300

    leitura e alteração categoria

--- CRUD-Categoria/view/formAlteracao.php
+++ CRUD-Categoria/view/formAlteracao.php
@@ -12,20 +12,25 @@ include_once "../controller/categoriaController.php";
 	<h1>Atualizar Categoria</h1>
 
     <?php
-    if (isset($mensagem)) {
-        echo '<div class="mensagem">' . $mensagem . '</div>';
-    }
+        if (isset($mensagem)) {
+            echo '<div class="mensagem">' . $mensagem . '</div>';
+        }
+
+        if (!empty($al)) {
     ?>
-	
-    <form action="../controller/categoriaController.php" method="POST">
-        <input type="hidden" name="action" value="atualizar">
-        <input type="hidden" name="idCategoria" value="<?php echo $al["idCategoria"];?>">
-        <label for="idCategoria">
-            Nome:
-        </label>
-        <input type="text" name="descricao" id="idCategoria" value="<?php echo $al["descricao"];?>">
-        <br>
-        <input type="submit" value="Ok">
-	</form>
+        <form action="../controller/categoriaController.php" method="POST">
+            <input type="hidden" name="action" value="atualizar">
+            <input type="hidden" name="idCategoria" value="<?php echo $al["idCategoria"];?>">
+            <label for="idCategoria">
+                Nome:
+            </label>
+            <input type="text" name="descricao" id="idCategoria" value="<?php echo $al["descricao"];?>">
+            <br>
+            <input type="submit" value="Ok">
+        </form>
+    <?php
+        }
+    ?>
+    <a href="../view/pageCategoria.php">Voltar para a lista de categorias</a>
 </body>
 </html>
