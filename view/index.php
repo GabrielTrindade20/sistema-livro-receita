a6e68b528b72ffbc71fc0976db3e9cba5e3843c0
unknown <patriciasuellen05@gmail.com>
2023-09-20 18:58:22 -0300

    U

--- view/index.php
+++ view/index.php
@@ -7,7 +7,7 @@ include_once('../controller/LoginController.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stylee.css">
+    <link rel="stylesheet" href="css/style.css">
     <link 
         rel="stylesheet" 
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
@@ -18,7 +18,7 @@ include_once('../controller/LoginController.php');
     <header>
             <section id="section-esquerda">
                 <div id="div-logo" id="section1">
-                    <a href="">
+                    <a href="css/">
                         <img src="" alt="logo">
                     </a>
                 </div>
