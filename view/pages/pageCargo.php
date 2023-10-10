db45be90bfe039119b1eb80e02cea474f907da2b
Gabriel <gabriel.rochat18@gmai.com>
2023-10-10 15:22:37 -0300

    atualização dos css das paginas

--- view/pages/pageCargo.php
+++ view/pages/pageCargo.php
@@ -19,7 +19,7 @@ $numCargos = count($cargos);
     <link rel="stylesheet" href="../css/stylePesq.css">
     <link rel="stylesheet" href="../css/styleTable.css">
     <link rel="icon" href="../css/iconsSVG/iconReceita.svg">
-    <link rel="stylesheet" href="../css/styleResponsiv.css">
+    <link rel="stylesheet" href="../css/styleResponsivo.css">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <title>Página de Receitas</title>
