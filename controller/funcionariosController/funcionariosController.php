cc9136115c5e4dcdd8595eecb859c25d82a887fe
GuilhermeRB1 <guilhermeeribeiro2@gmail.com>
2023-10-17 17:02:18 -0300

    corringindo erros de digitação

--- controller/funcionariosController/funcionariosController.php
+++ controller/funcionariosController/funcionariosController.php
@@ -2,14 +2,14 @@
 include_once('../../configuration/connect.php');
 include_once('../../model/modelFuncionarios/funcionariosModel.php'); 
 
-$funcionarioModel = new funcionariosModel($conexao);
+$funcionariosModel = new funcionariosModel($conexao);
 
 if (isset($_POST['salvar'])) {
     $nome = $_POST['nome'];
     $cargo = $_POST['cargo']; 
     $dataIngresso = $_POST['dataIngresso'];
     $status = $_POST['status']; 
-    $resultado = $funcionarioModel->criarFuncionario($nome, $cargo, $dataIngresso, $status);
+    $resultado = $funcionariosModel->criarFuncionario($nome, $cargo, $dataIngresso, $status);
 
     if ($resultado) {
         $mensagem = 'Funcionário cadastrado com sucesso!';
