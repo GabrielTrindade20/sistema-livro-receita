cfaadf9234660366f963cae0b1a35201cdb7ecd2
unknown <patriciasuellen05@gmail.com>
2023-11-15 16:08:44 -0300

    cadastro receita com foto

--- view/pages/Receitas/pageReceitaCadastro.php
+++ view/pages/Receitas/pageReceitaCadastro.php
@@ -3,6 +3,52 @@ include_once('../../../controller/protect.php');
 include_once('../../../configuration/connect.php');
 include '../../../model/funcoes.php';
 include_once('../../../controller/receitaController.php');
+include_once('../../../model/fotoModel.php');
+
+$fotoModel = new fotoModel($link);
+$usuario = $_SESSION["id"];
+
+$ultima_foto = $foto_recuperada = $fotoModel->recuperaFoto();
+
+// SALVAR FOTO
+if (isset($_FILES["foto_receita"]) && $_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["upload"])) {
+    $arquivo = $_FILES["foto_receita"];
+    if ($arquivo['error'])
+        $_SESSION["mensagem"] = "Falha ao enviar o arquivo";
+
+    if ($arquivo['size'] > 2097154)
+        $_SESSION["mensagem"] = "Arquivo muito grande! Max = 2MB";
+
+    $pasta = "../../../arquivos/";
+    $nome_do_arquivo = $arquivo['name'];
+    $novo_nome_arquivo = uniqid();
+    $extensao = strtolower(pathinfo($nome_do_arquivo, PATHINFO_EXTENSION));
+
+    if ($extensao != "jpg" && $extensao != "png")
+        $_SESSION["mensagem"] = "Tipo de arquivo não aceito";
+
+    $path =   $pasta . $novo_nome_arquivo . '.' . $extensao;
+    $deu_certo = move_uploaded_file($arquivo["tmp_name"], $path);
+
+    if ($deu_certo) {
+        $fotoModel->create($novo_nome_arquivo, $nome_do_arquivo, $path, $usuario);
+        $_SESSION["cadastro_sucesso"] = true;
+        $_SESSION["mensagem"] = "Arquivo salvo!";
+    } else{
+        $_SESSION["mensagem"] = "Erro ao enviar.";
+        $_SESSION["cadastro_sucesso"] = false;
+    }
+ 
+}
+
+if(isset($ultima_foto)) {
+$id_foto_receita =  $ultima_foto['id_foto_receita'];
+$img_ultima = "<img src=" . $ultima_foto['path'] . ">";
+$link_img = "<a targer=\"blank\" href=" . $ultima_foto['path'] . ">VER</a>";
+$delete_link_img = "<a href=\"pageReceitaCadastro.php?idFoto=". $id_foto_receita. "\&acao=deletar-foto\">DELETAR</a>";
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +58,11 @@ include_once('../../../controller/receitaController.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styleHomePage.css">
-    <link rel="stylesheet" href="../css/styleMenu.css">
+    <!-- BOOSTRAP  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+    <link rel="stylesheet" href="../../css/styleAll.css">
     <!-- <link rel="stylesheet" href="../../css/styleEdica.css"> -->
     <link rel="icon" href="../../css/iconsSVG/iconReceita.svg">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
@@ -35,7 +84,6 @@ include_once('../../../controller/receitaController.php');
             <?php
             if (isset($_SESSION["erros"])) {
                 $erros = $_SESSION["erros"];
-                // Exibir as mensagens de erro
                 foreach ($erros as $erro) {
                     echo $erro . "<br>";
                 }
@@ -53,11 +101,27 @@ include_once('../../../controller/receitaController.php');
 
         <!-- Formulário de Cadastro -->
         <div class="conteiner-abas">
-            <form method="POST" action="../../../controller/receitaController.php">
+            <div>
+                <?php
+                if (isset($_SESSION["mensagem"])) {
+                    echo $_SESSION["mensagem"];
+                    unset($_SESSION["mensagem"]); // Limpa a mensagem da sessão para que ela não seja exibida novamente
+                }
+                                  
+                if (isset($_SESSION["cadastro_sucesso"]) &&  $_SESSION["cadastro_sucesso"]) {
+                    echo $img_ultima; 
+                    unset($_SESSION["cadastro_sucesso"]);
+                }
+               ?>
+            </div>
+            <form enctype="multipart/form-data" action="" method="post">
                 <div class="box-foto">
                     <label for="foto_receita">Foto da Receita</label>
                     <input type="file" accept="image/*" name="foto_receita">
+                    <button type="submit" name="upload" class="button">Salvar</button>
                 </div>
+            </form>
+            <form method="POST" action="../../../controller/receitaController.php">
                 <div class="conteiner-dados">
                     <label for="nome">Nome</label>
                     <input type="text" id="nome" name="nome" required>
@@ -69,7 +133,7 @@ include_once('../../../controller/receitaController.php');
                     <input type="number" id="qtd_porcao" name="qtd_porcao" required>
                     <br>
                     <label for="degustador">Degustador</label>
-                        <?php monta_select_degustador(); ?>
+                    <?php monta_select_degustador(); ?>
                     <br>
                     <label for="nota_degustacao">Nota</label>
                     <input type="number" id="nota_degustacao" name="nota_degustacao">
@@ -87,7 +151,7 @@ include_once('../../../controller/receitaController.php');
                     <?php monta_select_cozinheiro(); ?>
                     <br>
                     <label for="categoria">Categoria</label>
-                        <?php monta_select_categoria(); ?>
+                    <?php monta_select_categoria(); ?>
                     <br>
                     <div class="box-text-area">
                         <h3 class="titulo">Modo de Preparo</h3>
@@ -140,12 +204,6 @@ include_once('../../../controller/receitaController.php');
             </div>
 
         </div>
-    </section>
-
-    <section align="right">
-        
-
-        
     </section>
 
 </body>
