b300c461fe029719516d4d003c6322e2ce85625e
Gabriel <gabriel.rochat18@gmai.com>
2023-11-15 21:59:27 -0300

    configuração dos stylos

--- view/pages/homePage.php
+++ view/pages/homePage.php
@@ -12,7 +12,7 @@ include_once('../../controller/protect.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     
-    <link rel="stylesheet" href="../css/styleAll.css">
+    <link rel="stylesheet" href="../css/styleConteudoPages.css">
     <link rel="stylesheet" href="../css/styleHomePage1.css">
     <link rel="icon" href="../css/iconsSVG/iconReceita.svg">
 
--- view/pages/pageFuncionario.php
+++ view/pages/pageFuncionario.php
@@ -13,12 +13,21 @@ include_once('../../controller/controllerFuncionario/funcionarioController.php')
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/stylePesq.css">
-    <link rel="stylesheet" href="../css/styleTable.css">
+    <!-- BOOSTRAP  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+
+    <link rel="stylesheet" href="../css/styleConteudoPages.css">
+    <link rel="stylesheet" href="../css/styleCabeçalhoPesquisa.css">
+    <link rel="stylesheet" href="../css/stylePesquisar.css">
+    <link rel="stylesheet" href="../css/styleTable1.css">
     <link rel="icon" href="../css/iconsSVG/iconReceita.svg">
-    <link rel="stylesheet" href="../css/stylePesq.css">
+    <link rel="icon" href="../css/iconsSVG/iconReceita.svg">
     <link rel="stylesheet" href="../css/styleResponsivo.css">
-    <link rel="icon" href="../css/iconsSVG/iconReceita.svg">
+
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
@@ -73,54 +82,57 @@ include_once('../../controller/controllerFuncionario/funcionarioController.php')
     <!-- Menu lateral - vem de outra página -->
     <?php require_once('../components/menu.php'); ?>
 
-    <div class="paginação">
-        <a href="homePage.php">Homepage > </a>
-        <a href="pagefuncionario.php">funcionario</a>
-    </div>
+    <section class="conteiner-conteudo2">
 
-    <section class="conteiner-pesquisa">
-        <div class="titulos" id="titulo">
-            <div class="conteiner-titulo">
-                <div>
-                    <h1>Lista de Funcionários</h1>
+        <div class="paginação">
+            <a href="homePage.php">Homepage > </a>
+            <a href="pagefuncionario.php">funcionario</a>
+        </div>
+
+        <div class="containerPesquisa">
+            <div class="row">
+                <div class="col-md-6 col-sm-12 conteiner-info">
+                    <div>
+                        <h1>Lista de Funcionários</h1>
+                    </div>
+
+                    <div class="info-qtd">
+                        <a href="#">
+                            <?php echo "(" . $countFuncionarios . ") Funcionários"; ?>
+                        </a>
+                    </div>
                 </div>
 
-                <div class="info-receitas">
-                    <a href="#">
-                        <?php echo "(" . $countFuncionarios . ") Funcionários"; ?>
-                    </a>
-                </div>
-            </div>
-
-            <div class="search-container">
-                <!-- Search -->
-                <div class="search-box">
-                    <form method="POST" action="">
-                        <div class="search-box-input-container">
-                            <input type="text" class="search-box-input" name="busca" placeholder="Pesquisar">
-                            <button type="submit" class="search-box-button">
-                                <span class="material-symbols-outlined">search</span>
-                                <!-- Substituído "Enviar" pelo ícone de lupa -->
-                            </button>
-                        </div>
+                <div class="col-md-6 col-sm-12 conteiner-func">
+                    <!-- Search -->
+                    <form class="form-p">
+                        <button>
+                            <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img"
+                                aria-labelledby="search">
+                                <path
+                                    d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
+                                    stroke="currentColor" stroke-width="1.333" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                            </svg>
+                        </button>
+                        <input class="input-p" placeholder="Pesquisar" required="" type="search">
                     </form>
-                </div>
 
-                <!-- Criar -->
-                <div class="button-nova">
-                    <a href="./Funcionario/pageFuncionarioCadastro.php?acao=cadastro">
-                        <button class="nova-receita-button">Cadastrar</button>
-                    </a>
+                    <!-- Criar -->
+                    <div class="button-nova">
+                        <a href="./Funcionario/pageFuncionarioCadastro.php?acao=cadastro">
+                            <button class="nova-button">Cadastrar</button>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="conteiner-button-inativar">
-            <button class="inativar-button" onclick="confirmarExclusaoCheckbox()">Inativar Selecionados</button>
-        </div>
+            <div class="conteiner-button-inativar">
+                <button class="inativar-button" onclick="confirmarExclusaoCheckbox()">Inativar Selecionados</button>
+            </div>
     </section>
 
-    <section class="conteiner-conteudo">
+    <section class="conteiner-conteudo2">
         <form id="inativarSelecionados" action="../../controller/funcionarioController.php?acao=inativosSelecionados"
             method="post">
             <table class="table center-table" style="margin-bottom: 5rem" border="1">
@@ -181,7 +193,8 @@ include_once('../../controller/controllerFuncionario/funcionarioController.php')
                                 <a
                                     href="../pages/Funcionario/pageFuncionarioAlteracao.php?idFuncionario=<?php echo $funcionario['idFuncionario']; ?>">
                             <td>
-                                <a href="../pages/Funcionario/pageFuncionarioAlteracao.php?idFuncionario=<?php echo $funcionario['idFuncionario']; ?>&acao=alteracao">
+                                <a
+                                    href="../pages/Funcionario/pageFuncionarioAlteracao.php?idFuncionario=<?php echo $funcionario['idFuncionario']; ?>&acao=alteracao">
                                     <span class="material-symbols-outlined"> edit </span>
                                 </a>
 
@@ -196,6 +209,11 @@ include_once('../../controller/controllerFuncionario/funcionarioController.php')
             </table>
         </form>
     </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
