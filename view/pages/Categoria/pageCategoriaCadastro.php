5528ba5967196b182eefa998f0cd30cc91f3d59b
unknown <patriciasuellen05@gmail.com>
2023-11-19 15:08:24 -0300

    style cargo - otimização do projeto

--- view/pages/Categoria/pageCategoriaCadastro.php
+++ view/pages/Categoria/pageCategoriaCadastro.php
@@ -4,45 +4,52 @@ include_once('../../../controller/protect.php');
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/styleEdica.css">
-    <link rel="icon" href="../../css/iconsSVG/iconReceita.svg">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <title>Categoria</title>
+    <link rel="stylesheet" href="../../css/styleAllConteinerPages.css">
+    <link rel="stylesheet" href="../../css/styleEdicao.css">
+    <link rel="icon" href="css/iconsSVG/iconReceita.svg">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>Categoria Cadastro</title>
 </head>
 
 <body>
     <!-- Menu lateral - vem de outra página -->
-    <?php require_once('../../components/menuSubFolders.php'); ?>  
+    <?php require_once('../../components/menuSubFolders2.php'); ?>
 
-    <section class="conteiner-conteudo">
-        <h1 class="titulo">Categoria</h1>
-
+    <section class="conteiner-conteudo-cadastro">
+        <div class="paginação-sub">
+            <a href="homePage.php">Homepage </a> >
+            <a href="../pageCategoria.php"> Categorias </a> >
+            <a href="#" class="pagina-atual"> Categoria Cadastro</a>
+        </div>
         <div class="conteiner-abas">
+            <div class="title-container">
+                <h1>Categoria</h1>
+            </div>
             <!-- Formulário de Cadastro -->
             <form method="POST" action="../../../controller/categoriaController.php">
 
-                <div class="conteiner-dados">
-                    <label for="nome">Nome:</label>
+                <div class="conteiner-dados-funcionario">
+                    <label for="nome">Nome</label>
                     <input type="text" id="nome" name="descricao" required>
                 </div>
-                <br>
 
                 <div class="conteiner-operacoes">
                     <!-- Botão para salvar o cargo -->
                     <button type="submit" name="salvar" class="button">Salvar</button>
 
                     <!-- Botão para cancelar e voltar à página principal -->
-                    <a href="../../pages/pageCategoria.php">Cancelar</a>
+                    <a href="../pageCategoria.php">Cancelar</a>
                 </div>
             </form>
         </div>
 
     </section>
-
 </body>
+
 </html>
--- view/pages/pageCategoria.php
+++ view/pages/pageCategoria.php
@@ -20,14 +20,16 @@ if (isset($_SESSION['resultados_pesquisa'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOSTRAP  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
-    <link rel="stylesheet" href="../css/stylePesq.css">
-    <link rel="stylesheet" href="../css/styleTable.css">
-    <link rel="stylesheet" href="../css/styleResponsivo.css">
+    <link rel="stylesheet" href="../css/styleAllConteinerPages.css">
+    <link rel="stylesheet" href="../css/styleCabecalhoPesquisa.css">
+    <link rel="stylesheet" href="../css/styleNoti.css">
+    <link rel="stylesheet" href="../css/stylePesquisar.css">
+    <link rel="stylesheet" href="../css/styleTable1.css">
     <link rel="icon" href="../css/iconsSVG/iconReceita.svg">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-
     <title>Categorias</title>
 
     <script>
@@ -41,6 +43,7 @@ if (isset($_SESSION['resultados_pesquisa'])) {
                 // Se o usuário cancelar, não faça nada
             }
         }
+
         function confirmarExclusaoCheckbox() {
             if (confirm("Tem certeza de que deseja excluir as categorias selecionadas?")) {
                 document.forms["excluirSelect"].submit();
@@ -49,131 +52,131 @@ if (isset($_SESSION['resultados_pesquisa'])) {
     </script>
 
 </head>
-<!-- Menu lateral - vem de outra página -->
-<?php require_once('../components/menu.php'); ?>
 
-<div class="paginação">
-    <a href="homePage.php">Homepage > </a>
-    <a href="pageCategoria.php">Categoria</a>
-</div>
+<body>
+    <!-- Menu lateral - vem de outra página -->
+    <?php require_once('../components/menu.php'); ?>
 
-<section class="conteiner-pesquisa">
-    <div class="titulos" id="titulo">
-        <div class="conteiner-titulo">
-            <div>
-                <h1>Lista de Categorias</h1>
-            </div>
-
-            <div class="info-receitas">
-                <a href="#">
-                    <?php echo "(" . $countCategorias . ") Categorias"; ?>
-                </a>
-            </div>
+    <section class="conteiner-conteudo">
+        <div class="paginação">
+            <a href="homePage.php">Homepage </a> >
+            <a href="#" class="pagina-atual">Categoria</a>
         </div>
-
-        <div class="search-container">
-            <div class="search-box">
-                <form method="post" action="../../controller/categoriaController.php">
-                    <div class="search-box-input-container">
-                        <input type="text" name="descricao" class="search-box-input" placeholder="Pesquisar...">
-                        <button type="submit" class="search-box-button"><i
-                                class="search-box-icone icon icon-search"></i></button>
+        <div class="containerPesquisa">
+            <div class="row">
+                <div class="col-md-6 col-sm-12 conteiner-info">
+                    <div>
+                        <h1>Lista de Categorias</h1>
                     </div>
-                </form>
-            </div>
-            <!-- Criar -->
-            <div class="button-nova">
-                <a href="./Categoria/pageCategoriaCadastro.php">
-                    <button class="nova-receita-button">Cadastrar</button>
-                </a>
+
+                    <div class="info-qtd">
+                        <a href="#">
+                            <?php echo "(" . $countCategorias . ") Categorias"; ?>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12 conteiner-func">
+                    <form method="post" action="../../controller/categoriaController.php" class="form-p">
+                        <button>
+                            <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="search">
+                                <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                        </button>
+                        <input class="input-p" placeholder="Pesquisar" required="" type="search" name="descricao">
+                    </form>
+
+                    <!-- Criar -->
+                    <div class="button-nova">
+                        <a href="./Categoria/pageCategoriaCadastro.php">
+                            <button class="nova-button">Cadastrar</button>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-
-    <!-- Notificação de erro ou não -->
-    <div class="mensagens">
-        <?php
-        if (isset($_SESSION["erros"])) {
-            $erros = $_SESSION["erros"];
-            // Exibir as mensagens de erro
-            foreach ($erros as $erro) {
-                echo $erro . "<br>";
+        <!-- Notificação de erro ou não -->
+        <div class="mensagens">
+            <?php
+            if (isset($_SESSION["erros"])) {
+                $erros = $_SESSION["erros"];
+                // Exibir as mensagens de erro
+                foreach ($erros as $erro) {
+                    echo $erro . "<br>";
+                }
+                // Limpar as mensagens de erro da sessão
+                unset($_SESSION["erros"]);
+            } elseif (isset($_SESSION["sucesso"])) {
+                $sucessos = $_SESSION["sucesso"];
+                foreach ($sucessos as $sucesso) {
+                    echo $sucesso . "<br>";
+                }
+                unset($_SESSION["sucesso"]);
             }
-            // Limpar as mensagens de erro da sessão
-            unset($_SESSION["erros"]);
-        } elseif (isset($_SESSION["sucesso"])) {
-            $sucessos = $_SESSION["sucesso"];
-            foreach ($sucessos as $sucesso) {
-                echo $sucesso . "<br>";
-            }
-            unset($_SESSION["sucesso"]);
-        }
-        ?>
-    </div>
-</section>
+            ?>
+        </div>
 
-<section class="conteiner-conteudo">
-    <button onclick="confirmarExclusaoCheckbox()">Excluir Selecionados</button>
+    </section>
 
-    <form id="excluirSelect" action="../../controller/categoriaController.php?acao=excluirSelecionados" method="post">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th class="select-column">-</th>
-                    <th>Categorias</th>
-                    <th class="operacao">Operações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- Verifica se houve pesquisa e exibe resultados ou mostra todas as categorias -->
-                <?php if (isset($resultados) && !empty($resultados)): ?>
-                    <?php foreach ($resultados as $categoria): ?>
-                        <tr>
-                            <td class="select-column">
-                                <input type="checkbox" name="checkbox[]" value="<?php echo $categoria['idCategoria']; ?>">
-                            </td>
-                            <td>
-                                <?php echo $categoria['descricao']; ?>
-                            </td>
-                            <!-- Operações -->
-                            <td>
-                                <a
-                                    href="../pages/Categoria/pageCategoriaAlteracao.php?idCategoria=<?php echo $categoria['idCategoria']; ?>">
-                                    <span class="material-symbols-outlined"> edit </span>
-                                </a>
-                                <a href="#" onclick="confirmarExclusao(<?php echo $categoria['idCategoria']; ?>);"
-                                    class="button">
-                                    <span class="material-symbols-outlined"> delete </span>
-                                </a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <?php foreach ($categorias as $index => $categoria): ?>
-                        <tr class="<?php echo ($index % 2 == 0) ? 'even-row' : 'odd-row'; ?>">
-                            <td class="select-column">
-                                <input type="checkbox" name="checkbox[]" value="<?php echo $categoria['idCategoria']; ?>">
-                            </td>
-                            <td>
-                                <?php echo $categoria['descricao']; ?>
-                            </td>
-                            <td>
-                                <a
-                                    href="../pages/Categoria/pageCategoriaAlteracao.php?idCategoria=<?php echo $categoria['idCategoria']; ?>">
-                                    <span class="material-symbols-outlined"> edit </span>
-                                </a>
-                                <a href="#" onclick="confirmarExclusao(<?php echo $categoria['idCategoria']; ?>);"
-                                    class="button">
-                                    <span class="material-symbols-outlined"> delete </span>
-                                </a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </tbody>
-        </table>
-    </form>
-</section>
+    <section class="conteiner-conteudo2">
+        <div class="conteiner-button-inativar">
+            <button onclick="confirmarExclusaoCheckbox()">Excluir Selecionados</button>
+        </div>
+
+        <form id="excluirSelect" action="../../controller/categoriaController.php?acao=excluirSelecionados" method="post">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th class="select-column">-</th>
+                        <th>Categorias</th>
+                        <th class="operation">Operações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Verifica se houve pesquisa e exibe resultados ou mostra todas as categorias -->
+                    <?php if (isset($resultados) && !empty($resultados)) : ?>
+                        <?php foreach ($resultados as $categoria) : ?>
+                            <tr>
+                                <td class="select-column">
+                                    <input type="checkbox" name="checkbox[]" value="<?php echo $categoria['idCategoria']; ?>">
+                                </td>
+                                <td>
+                                    <?php echo $categoria['descricao']; ?>
+                                </td>
+                                <!-- Operações -->
+                                <td class="operation">
+                                    <a href="../pages/Categoria/pageCategoriaAlteracao.php?idCategoria=<?php echo $categoria['idCategoria']; ?>">
+                                        <span class="material-symbols-outlined"> edit </span>
+                                    </a>
+                                    <a href="#" onclick="confirmarExclusao(<?php echo $categoria['idCategoria']; ?>);" class="button">
+                                        <span class="material-symbols-outlined"> delete </span>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php else : ?>
+                        <?php foreach ($categorias as $index => $categoria) : ?>
+                            <tr>
+                                <td class="select-column">
+                                    <input type="checkbox" name="checkbox[]" value="<?php echo $categoria['idCategoria']; ?>">
+                                </td>
+                                <td>
+                                    <?php echo $categoria['descricao']; ?>
+                                </td>
+                                <td class="operation">
+                                    <a href="../pages/Categoria/pageCategoriaAlteracao.php?idCategoria=<?php echo $categoria['idCategoria']; ?>">
+                                        <span class="material-symbols-outlined"> edit </span>
+                                    </a>
+                                    <a href="#" onclick="confirmarExclusao(<?php echo $categoria['idCategoria']; ?>);" class="button">
+                                        <span class="material-symbols-outlined"> delete </span>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </tbody>
+            </table>
+        </form>
+    </section>
 
 
 </body>
